37f0da75724b4f8977498686137d599b4c21a57a
Ninh Khong <ninhkhongdang@gmail.com>
2022-09-07 16:35:37 +0700

    feat: course update and end actions

--- classin/course.go
+++ classin/course.go
@@ -7,6 +7,8 @@ type courses struct {
 
 type Courses interface {
 	CreateCourse(courseName string, school int) (CourseResponse, error)
+	UpdateCourse(courseName string, courseId string, school int) (CourseResponse, error)
+	EndCourse(courseId string, school int) (CourseResponse, error)
 }
 
 // Courses constructor (from Client)
@@ -21,12 +23,31 @@ type CourseResponse struct {
 	ErrorInfo ErrorInfo `json:"error_info"`
 }
 
-// AddStudentToSchool POST
+// CreateCourse POST
 func (c courses) CreateCourse(courseName string, school int) (CourseResponse, error) {
 	var r CourseResponse
 	params := map[string]string{
 		"courseName": courseName,
 	}
 	err := c.client.request("POST", "/partner/api/course.api.php?action=addCourse", params, &r, school)
+	return r, err
+}
+
+func (c courses) UpdateCourse(courseName string, courseId string, school int) (CourseResponse, error) {
+	var r CourseResponse
+	params := map[string]string{
+		"courseName": courseName,
+		"courseId":   courseId,
+	}
+	err := c.client.request("POST", "/partner/api/course.api.php?action=editCourse", params, &r, school)
+	return r, err
+}
+
+func (c courses) EndCourse(courseId string, school int) (CourseResponse, error) {
+	var r CourseResponse
+	params := map[string]string{
+		"courseId": courseId,
+	}
+	err := c.client.request("POST", "/partner/api/course.api.php?action=endCourse", params, &r, school)
 	return r, err
 }
--- classin/course_test.go
+++ classin/course_test.go
@@ -2,6 +2,7 @@ package classin
 
 import (
 	"errors"
+	"strconv"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -12,6 +13,18 @@ func TestCourses(t *testing.T) {
 	courseName := "MRT IELTS"
 
 	result, _ := classinClient.Courses().CreateCourse(courseName, 1)
+	if result.ErrorInfo.ErrNo != 1 && result.ErrorInfo.ErrNo != 135 {
+		assert.Error(t, errors.New(result.ErrorInfo.Error))
+	}
+
+	newCourseName := "MRS IELTS"
+	courseId := result.Data
+	result, _ = classinClient.Courses().UpdateCourse(newCourseName, strconv.Itoa(courseId), 1)
+	if result.ErrorInfo.ErrNo != 1 && result.ErrorInfo.ErrNo != 135 {
+		assert.Error(t, errors.New(result.ErrorInfo.Error))
+	}
+
+	result, _ = classinClient.Courses().EndCourse(strconv.Itoa(courseId), 1)
 	if result.ErrorInfo.ErrNo != 1 && result.ErrorInfo.ErrNo != 135 {
 		assert.Error(t, errors.New(result.ErrorInfo.Error))
 	}
